44dc5bcb0f15cea3774d9f2d93e8296287f31d4a
MAQBOOL THOUFEEQ THARAYIL <maqboolthoufeeq.t@gmail.com>
2022-08-17 09:41:56 +0200

    Feature: Register, Update, Login profile serializer

--- apps/ratings/serializers.py
+++ apps/ratings/serializers.py
@@ -3,17 +3,17 @@ from tkinter.tix import Tree
 from rest_framework import serializers
 from .models import Rating
 
-class RatingsErializer(serializers.ModelSerializer):
+
+class RatingSerializer(serializers.ModelSerializer):
     rater = serializers.SerializerMethodField(read_only=True)
     agent = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Rating
         exclude = ["updated_at", "pkid"]
-    
+
     def get_rater(self, obj):
         return obj.rater.username
-    
+
     def get_agent(self, obj):
         return obj.agent.user.username
-    
