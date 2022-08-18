ea3ff767f647868be0d8c86d1ca9502bd0e22d65
MAQBOOL THOUFEEQ THARAYIL <maqboolthoufeeq.t@gmail.com>
2022-08-18 10:00:49 +0200

    Feature: Property model Serializer, exceptions

---
+++ apps/properties/serializers.py
@@ -0,0 +1,30 @@
+from django_countries.serializer_fields import CountryField
+from django_countries.serializers import CountryFieldMixin
+from rest_framework import serializers
+from .models import Property, PropertyViews
+
+
+class PropertySerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    country = CountryField(name_only=True)
+
+    class Meta:
+        model = Property
+        exclude = ['pkid', 'updated_at', 'objects', 'published']
+
+    def get_user(self, obj):
+        return obj.user.username
+
+
+class PropertyCreateSerializer(serializers.ModelSerializer):
+    country = CountryField(name_only=True)
+
+    class Meta:
+        model = Property
+        exclude = ['updated_at', 'pkid']
+
+
+class PropertyViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PropertyViews
+        exclude = ['updated_at', 'pkid']
